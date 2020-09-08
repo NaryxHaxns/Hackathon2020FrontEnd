7af668bb3de9325d01065e396830b1e574a6e07f
Ryan Nash <nashkbry@gmail.com>
2020-09-08 15:30:56 -0700

    Setting up the environment

---
+++ src/pages/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Election 2020</h1>
+        </header>
+      </div>
+    );
+  }
+
+}
